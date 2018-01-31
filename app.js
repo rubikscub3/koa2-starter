849b5dfce7dd31d6e820dc6df93f0a30f27baf66
unknown <aatir.farooqui@gmail.com>
2018-01-31 13:38:01 +0500

    second commit

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const Koa = require('koa');
 const Router = require('koa-router');
+const BodyParser = require('koa-bodyparser');
+const logger = require('koa-logger');
 
 const app = new Koa();
 const router = new Router();
 
-router.get("/", async function(ctx)  {
-    let name = ctx.request.query.name || "World";
-    ctx.body = {message : `Hello ${name}!`}
-});
-
+app.use(logger());
+app.use(BodyParser());
 app.use(router.routes()).use(router.allowedMethods());
 app.listen(3000);
+
+
+
+router.get("/", async function(ctx)  {
+    let name = ctx.request.body.name || "World";
+    ctx.body = {message : `Hello ${name}!`}
+});
